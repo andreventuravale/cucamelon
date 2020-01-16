7a1e2770d5fc263aaf75f390e64ef19da0624114
andreventuravale <andreventuravale@gmail.com>
2020-01-15 21:19:36 -0300

    Refactoring

--- test/exported/scenario.test.js
+++ test/exported/scenario.test.js
@@ -6,10 +6,6 @@ suite('Exported / Scenario template function', () => {
     this.fakeRun = td.func()
     this.fakeSuite = td.func()
 
-    td.when(
-      this.fakeRun(require('./metadata.json'))
-    ).thenReturn('the return from run(metadata)')
-
     this.Scenario = rewiremock.proxy('../../lib/Scenario', {
       './run-metadata': this.fakeRun,
       './suite': () => this.fakeSuite
@@ -19,6 +15,10 @@ suite('Exported / Scenario template function', () => {
   teardown(() => td.reset())
 
   test('Calls the suite definition as expected and delegates execution to the run function', function () {
+    td.when(
+      this.fakeRun(require('./metadata-with-summary.json'))
+    ).thenReturn('the return from run(metadata)')
+
     this.Scenario([
       ': Aute et laborum laboris laborum pariatur laboris velit ea aliquip ut dolore cillum.',
       '',
@@ -34,10 +34,31 @@ suite('Exported / Scenario template function', () => {
       this.fakeSuite(
         [
           `Scenario: Aute et laborum laboris laborum pariatur laboris velit ea aliquip ut dolore cillum.`,
-          ``,
           `      Culpa nisi tempor sint voluptate cupidatat laborum ex duis non duis aliqua enim ex irure.`,
-          `      Minim sit Lorem incididunt reprehenderit aliqua cupidatat id aliquip consequat incididunt dolore in.`,
-          ``
+          `      Minim sit Lorem incididunt reprehenderit aliqua cupidatat id aliquip consequat incididunt dolore in.`
+        ].join('\n'),
+        'the return from run(metadata)'
+      )
+    )
+  })
+
+  test('Calls the suite definition as expected and delegates execution to the run function', function () {
+    td.when(
+      this.fakeRun(require('./metadata-without-summary.json'))
+    ).thenReturn('the return from run(metadata)')
+
+    this.Scenario([
+      ': Aute et laborum laboris laborum pariatur laboris velit ea aliquip ut dolore cillum.',
+      '',
+      'Given Laborum ut exercitation laborum anim enim ad sit dolore id.',
+      'And Dolor mollit nulla sit cupidatat nostrud adipisicing laboris eu consectetur sit minim.',
+      'But Dolor elit elit incididunt consequat esse elit.'
+    ].map(ln => `${ln}\n`))
+
+    td.verify(
+      this.fakeSuite(
+        [
+          `Scenario: Aute et laborum laboris laborum pariatur laboris velit ea aliquip ut dolore cillum.`
         ].join('\n'),
         'the return from run(metadata)'
       )
